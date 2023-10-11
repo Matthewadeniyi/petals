6765d697e06f4e14b841e959c7ae6fc947da8f8a
Matthewadeniyi <matthewadeniyi55@gmail.com>
2023-10-11 17:23:08 +0100

    implemented profile-picture updates and session creation

--- Chat Application/upload.php
+++ Chat Application/upload.php
@@ -1,60 +1,19 @@
 <?php 
+session_start();ob_start();
  include ("myclass.php");
-// $sn = $_GET['sn'];
-$target_dir = "upload/";
-@$file_name =  basename($_FILES["fileToUpload"]["name"]);
-@$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION)); 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        $report  =  "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        $report = "File is not an image.";$count = 1;
-        $uploadOk = 0;
-    }
+ $id = $_SESSION['id'];
 
-    // Check if file already exists
-if (file_exists($target_file)) {
-    $report = "Sorry, file already exists."; $count=1;
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 50000000) {
-    $report = "Sorry, your file is too large.";$count=1;
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    $report = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";$count=1;
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    $report = "Sorry, your file was not uploaded.";$count=1;
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {    
-        
-    $sql = $db->query("UPDATE students SET picture = '$file_name' WHERE sn = '$sn' ") or die($db->error);
+ if(isset($_POST['change'])){
+    $target = 'upload/';
+    $targetfile $target.$_FILES['picture']['name'];
 
-    if($sql){
-        $report = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    }else{
-        die($db->error);
-        unlink('upload/'.$file_name);
-    }
-    } else {
-        $report = "Sorry, there was an error uploading your file.";$count=1;
-    }
-}
-}
-
+    move_uploaded_file($_FILES['picture']['tmp_name'],$targetfile);
+    $db->query("UPDATE user SET picture = '$targetfile' WHERE id='$id' ");
+    echo 'Sucess'.$_SESSION['id'];
+ }
+ 
  ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -81,10 +40,10 @@ if ($uploadOk == 0) {
 <div class="container">
         <div class="card mx-auto">
           <div class="card-header">
-            <h4>Account</h4>
+            <h4>Upload picture</h4>
           </div>
           <div class="card-body">
-            <form action="" method="POST">
+            <form action="" method="POST" enctype=''>
                 
                 <label for="">Email</label>
                 <div class="form-group">
